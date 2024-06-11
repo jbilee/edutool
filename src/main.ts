b21198506262d3e267688b9a989db27b84670254
jbilee <julishahn@gmail.com>
2024-06-11 14:21:18 +0900

    feat: Set up Vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { clerkPlugin } from "vue-clerk";
+import App from "./App.vue";
+import HomeView from "./views/HomeView.vue";
+import SignInView from "./views/SignInView.vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", name: "home", component: HomeView },
+  { path: "/signin", name: "signin", component: SignInView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(clerkPlugin, {
+  publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
+});
+app.mount("#app");
