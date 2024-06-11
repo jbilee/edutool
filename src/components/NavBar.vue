3576005fe89cce5e2eb96bbf7035eec49047d2f6
jbilee <julishahn@gmail.com>
2024-06-11 15:11:55 +0900

    feat: Sign out button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
+import { SignedIn, SignedOut, useClerk } from "vue-clerk";
+const { signOut } = useClerk()
+const onSignout = () => signOut()
 </script>
 
 <template>
   <div class="container">
-    <div class="logo">로고</div>
-    <RouterLink to="/signin">로그인</RouterLink> 설정
+    <div class="logo">
+      <RouterLink to="/">로고</RouterLink>
+    </div>
+    <SignedIn>
+      <button @click="onSignout">로그아웃</button>
+    </SignedIn>
+    <SignedOut>
+      <RouterLink to="/signin">로그인</RouterLink>
+    </SignedOut> 설정
   </div>
 </template>
 
 <style scoped>
 .container {
-  height: 70px;
   display: flex;
+  padding: 1.5rem;
 }
 
 .logo {
