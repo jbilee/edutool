cb42d0503d5e275095ac91c78210f6110aef2077
jbilee <julishahn@gmail.com>
2024-06-11 14:21:50 +0900

    feat: Navigation bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <div class="logo">로고</div>
+    <RouterLink to="/signin">로그인</RouterLink> 설정
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 70px;
+  display: flex;
+}
+
+.logo {
+  margin-right: auto;
+}
+</style>
