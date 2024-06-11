579ddee4c05a5dd9615fdb2e51c14decd4dbcc94
jbilee <julishahn@gmail.com>
2024-06-11 14:25:06 +0900

    feat: Sign-in and home views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-  <div>
-  </div>
+  <NavBar />
+  <RouterView />
 </template>
-
-<style scoped>
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>메인화면</div>
+</template>
