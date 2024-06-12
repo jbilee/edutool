fc6e3ac2b258599d998bd472a0c26a0983150302
jbilee <julishahn@gmail.com>
2024-06-12 09:51:45 +0900

    feat: Sidebar component on members' main page

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="sidebar">
+    <li>내 커리큘럼</li>
+    <li>숙제/과제</li>
+    <li>퀴즈</li>
+    <li>일정관리</li>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 100%;
+  padding: 1.5rem 0;
+  border-right: 1px solid rgb(225, 225, 225);
+  flex: 1 0 1;
+
+  @media (min-width: 768px) {
+    max-width: 210px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 260px;
+  }
+}
+</style>
