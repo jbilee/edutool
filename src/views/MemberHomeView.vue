24901eef0f6386087134e64eab8c8f36d5ae8b01
jbilee <julishahn@gmail.com>
2024-06-12 09:56:50 +0900

    feat: Main page for signed in members

---
+++ src/views/MemberHomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Sidebar from "../components/Sidebar.vue";
+import TeacherView from "./TeacherView.vue";
+
+</script>
+
+<template>
+  <div class="container">
+    <Sidebar />
+    <TeacherView />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: inherit;
+  scrollbar-gutter: stable both-edges;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+}
+</style>
