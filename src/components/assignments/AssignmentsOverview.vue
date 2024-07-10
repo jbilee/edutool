6d9cf77918dd5a9b3e03db8a4317e4a7105439ed
jbilee <julishahn@gmail.com>
2024-07-10 20:10:35 +0900

    feat: Assignments overview component

---
+++ src/components/assignments/AssignmentsOverview.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router"
+import type { Assignment } from "../../utils/DataStore.ts"
+
+const props = defineProps<{ assignments: Assignment[] }>()
+</script>
+
+<template>
+  <li v-for="assignment in props.assignments">
+    <RouterLink :to="`/assignments/${assignment.id}`">{{ assignment.title }}</RouterLink>
+    <span>{{ assignment.submissions }} / {{ assignment.classSize }} 제출</span>
+  </li>
+</template>
+
+<style scoped>
+li {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+
+  &:last-child {
+    margin: 0;
+  }
+}
+</style>
--- src/views/TeacherView.vue
+++ src/views/TeacherView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import WeeklyCalendar from "../components/calendar/WeeklyCalendar.vue"
+import AssignmentsOverview from "../components/assignments/AssignmentsOverview.vue"
 import EdupalAd from "../components/EdupalAd.vue"
+import WeeklyCalendar from "../components/calendar/WeeklyCalendar.vue"
+import { useDataStore } from "../utils/DataStore"
+
+const store = useDataStore()
 </script>
 
 <template>
@@ -12,7 +16,7 @@ import EdupalAd from "../components/EdupalAd.vue"
         <div class="card">
           <div class="card__title">주간 일정</div>
           <div class="card__content">
-            <WeeklyCalendar />
+            <WeeklyCalendar :assignments="store.assignments" />
           </div>
         </div>
         <div class="card">
@@ -27,9 +31,7 @@ import EdupalAd from "../components/EdupalAd.vue"
       <div class="card">
         <div class="card__title">과제 제출 현황</div>
         <div class="card__content">
-          <li>전쟁터 디오라마 프로젝트 (3/4 제출)</li>
-          <li>그랜드캐니언 5000자 에세이 (1/4 제출)</li>
-          <li>양파 식물일기 (1/4 제출)</li>
+          <AssignmentsOverview :assignments="store.assignments" />
         </div>
       </div>
     </div>
