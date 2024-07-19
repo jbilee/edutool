bc8cfc723f9abeac604cd2bb5efe71157901097c
jbilee <julishahn@gmail.com>
2024-07-19 19:03:24 +0900

    feat: Student view of curriculum page

--- src/views/CurriculumView.vue
+++ src/views/CurriculumView.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia"
+import { useAccountStore } from "../stores/AccountStore"
 import { useCurriculumStore } from "../stores/CurriculumStore"
 import ProgressBar from "../components/curriculums/ProgressBar.vue"
 
-const store = useCurriculumStore()
-const { curriculums } = storeToRefs(store)
+const accountStore = useAccountStore()
+const { accountType } = storeToRefs(accountStore)
+const curriculumStore = useCurriculumStore()
+const curriculums = curriculumStore.getCurriculumsById(accountStore.enrolledClasses)
 const handleClick = () => alert("clicked on icon")
 </script>
 
 <template>
-  <div>
+  <div v-if="accountType === 'tutor'">
     <h1>커리큘럼</h1>
     <div class="control-bar">
       <div class="card__title">진행 중인 커리큘럼</div>
@@ -23,6 +26,16 @@ const handleClick = () => alert("clicked on icon")
         :current-chapter="curriculum.currentChapter" />
     </div>
     <div class="card__title hidden">완료한 커리큘럼</div>
+  </div>
+  <div v-if="accountType === 'student'">
+    <h1>커리큘럼</h1>
+    <div class="card__title">학습 중인 커리큘럼</div>
+    <div v-for="curriculum in curriculums" class="card__content">
+      <h3>{{ curriculum.name }}</h3>
+      <p>{{ curriculum.goal }}</p>
+      <ProgressBar :chapters="curriculum.chapters" :total-chapters="curriculum.totalChapters" text-size="size-regular"
+        :current-chapter="curriculum.currentChapter" />
+    </div>
   </div>
 </template>
 
