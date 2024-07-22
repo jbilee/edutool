2c1bd71a4ba4d5c97fd6122e027e0fa6915c275d
jbilee <julishahn@gmail.com>
2024-07-22 15:08:24 +0900

    fix: Fix data for default enrolled classes

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -2,7 +2,6 @@ export const accountType = null;
 export const isLoggedIn = false;
 export const displayName = "";
 export const enrolledClasses = [
-  { id: 2, curriculum: "식물 키우기" },
+  { id: 2, curriculum: "수학: 미적분" },
   { id: 3, curriculum: "영어 - 해외여행" },
-  { id: 4, curriculum: "여행 - 학교생활" },
 ];
